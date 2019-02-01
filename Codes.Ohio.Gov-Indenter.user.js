0a1f6476ef32d46a7fedab935eb53e5ce9f97d2e
Jon Freed <jon-freed@users.noreply.github.com>
2019-02-01 17:13:11 -0500

    Change to work off of outline level prefixes
    The anchor tags ("a") this code worked off of are no longer there.  Changed code to use regular expressions to recognize outline level prefixes and indent accordingly.  Currently it will only indent to four levels, e.g.:  A-Z, 1-9, a-z, then roman numerals.  If I can find an example of deeper levels then I'll change the code to accommodate them.

--- Codes.Ohio.Gov-Indenter.user.js
+++ Codes.Ohio.Gov-Indenter.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name        Codes.Ohio.Gov-Indenter.user.js
 // @namespace   http://dmiratech.com/
-// @version     1.0.4
+// @version     1.0.5
 // @description Indents the divisions and subdivisions, etc. in Ohio's online
-//              copy of its laws (Revised Code; ORC) and Administrative Code 
+//              copy of its laws (Revised Code; ORC) and Administrative Code
 //              (OAC).
 // @author      Jonathan (Jon) Freed
 // @license     The Unlicense.  See http://unlicense.org/.
@@ -15,41 +15,73 @@
 // @run-at      document-idle
 // ==/UserScript==
 ///////////////////////////////////////////////////////////////////////////////
-var iPixelsOfIndentingPerLevel = 50;
+var iPixelsOfIndentingPerLevel = 25;
 ///////////////////////////////////////////////////////////////////////////////
 console.log("Starting script for indenting ORC and OAC content.");
 // If the document is loaded...
-if( document.readyState !== "loading" ) {
-  // Then do the indenting
-  vIndentContentByLevel();
-}
-else { // Have the browser wait until doc is loaded to do the indenting.
-  document.addEventListener('DOMContentLoaded', vIndentContentByLevel);
+if (document.readyState !== "loading") {
+    // Then do the indenting
+    vIndentContentByLevel();
+} else { // Have the browser wait until doc is loaded to do the indenting.
+    document.addEventListener('DOMContentLoaded', vIndentContentByLevel);
 }
 // Define the function that will do the indenting
 function vIndentContentByLevel() {
-  console.log("ORC/OAC-Indenting:  DOM content loaded; starting indenting.");
-  // The initial level of indenting is 0 pixels.
-  var sPaddingLeft = "0px";
-  // Get the div for content then its children, and then for each child...
-  var aX = document.querySelectorAll("div#doc > div.content > *");
-  for( iX=0; iX<aX.length; iX++) {
-    // If we're on a heading...
-    if( aX[iX].nodeName.slice(0,1) === "H" ) {
-      // reset the indenting to zero
-      sPaddingLeft = "0px";
-    }
-    // Else, if we're on a "p" that isn't a history "p"...
-    else if( aX[iX].matches('p:not([class^=History])') ) {
-      // Try to get the node's first "a" anchor and its "name" attribute value.
-      var sName = ((aY=aX[iX].querySelector("a")) === null ? undefined : aY.name);
-      // If we got a name value...
-      if( sName !== undefined ) {
-        // Determine this p's indenting value based on the name's number of "("
-        sPaddingLeft = ((sName.split("(").length-2)*iPixelsOfIndentingPerLevel)+"px";
-      }// (else we just use the value we already have for indenting
-      // Use the indenting value to indent (pad-left) the element
-      aX[iX].style.paddingLeft = sPaddingLeft;
-    }
-  }//end for(...)
-}//end vIndentContentByLevel()
+    console.log("ORC/OAC-Indenting:  DOM content loaded; BEGIN indenting.");
+
+    // The initial level of indenting is 0 pixels.
+    var sPaddingLeft = "0px";
+
+    // Get the div for content then its children, and then for each child...
+    var aX = document.querySelectorAll("div#doc > div.content > *");
+
+    // Declare/initialize variables to help us process those children
+    var reForOutlineLvlPrefix = /^\(.+\)[\W\s]/;
+    var aCurPrefixes = [];
+    var aReLvls = [
+        /^\([A-Z]+\)[\W\s]/
+        , /^\(\d+\)[\W\s]/
+        , /^\([a-z]+\)[\W\s]/
+        , /^\([ivxlcd]+\)[\W\s]/
+    ];
+
+    // For each of those children...
+    for (var iX = 0; iX < aX.length; iX++) {
+        // If we're on a heading...
+        if (aX[iX].nodeName.slice(0, 1) === "H") {
+            // reset everthing
+            sPaddingLeft = "0px";
+            aCurPrefixes = [];
+        }
+        // Else, if we're on a "p" that isn't a history "p"...
+        else if (aX[iX].matches('p:not([class^=History])')) {
+
+            // If the p begins with a outline level prefix
+            var reResult = reForOutlineLvlPrefix.exec(aX[iX].textContent);
+            if (reResult) {
+                var sCurPrefix = reResult[0];
+                // Determine the level of the current prefix
+                var iCurLvl;
+                for (var iTestLvl = 0; iTestLvl < aReLvls.length; iTestLvl++) {
+                    if (aReLvls[iTestLvl].test(sCurPrefix)) {
+                        iCurLvl = iTestLvl;
+                        break;
+                    }
+                }
+                if (iCurLvl === undefined) {
+                    console.log("ORC/OAC-Indenting:  Exiting! Cannot determine level of this outline prefix: \"" + sCurPrefix + "\"");
+                    return;
+                }
+                // Revise the array of previous prefixes so it only has levels lower than the current level
+                aCurPrefixes = aCurPrefixes.slice(0, iCurLvl);
+                // Append the current prefix
+                aCurPrefixes.push(sCurPrefix);
+            } //end if the p begins with an outline level prefix
+
+            // Use the number of levels to set the indenting (pad-left the current "p" element)
+            aX[iX].style.paddingLeft = ((aCurPrefixes.length - 1) * iPixelsOfIndentingPerLevel) + "px";
+
+        } //end else-if
+    } //end for(...)
+    console.log("ORC/OAC-Indenting:  END indenting.");
+} //end vIndentContentByLevel()
