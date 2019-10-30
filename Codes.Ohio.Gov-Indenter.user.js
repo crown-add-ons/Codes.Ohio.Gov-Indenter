7c046e6dde8223e2f9caed0e51ad93f7bd5eb72a
Jon Freed <jon-freed@users.noreply.github.com>
2019-10-30 00:28:52 -0400

    new approach to identifying current level

--- Codes.Ohio.Gov-Indenter.user.js
+++ Codes.Ohio.Gov-Indenter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Codes.Ohio.Gov-Indenter.user.js
 // @namespace   http://dmiratech.com/
-// @version     1.1.2
+// @version     2.0.1
 // @description Indents the divisions and subdivisions, etc. in Ohio's online
 //              copy of its laws (Revised Code; ORC) and Administrative Code
 //              (OAC).
@@ -17,7 +17,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 var iPixelsOfIndentingPerLevel = 50;
 ///////////////////////////////////////////////////////////////////////////////
-console.log("Starting script for indenting ORC and OAC content.");
+console.info("Starting script for indenting ORC and OAC content.");
 // If the document is loaded...
 if (document.readyState !== "loading") {
     // Then do the indenting
@@ -27,159 +27,144 @@ if (document.readyState !== "loading") {
 }
 // Define the function that will do the indenting
 function vIndentContentByLevel() {
-    console.log("ORC/OAC-Indenting:  DOM content loaded; BEGIN indenting.");
-
-    // The initial level of indenting is 0 pixels.
-    var sPaddingLeft = "0px";
+    console.info("ORC/OAC-Indenting:  DOM content loaded; BEGIN indenting.");
 
     // Get the div for content then its children, and then for each child...
     var aX = document.querySelectorAll("div#doc > div.content > *");
 
     // Declare/initialize variables to help us process those children
-    var reForOutlineLvlPrefix = /^[\W\s]*\((.+)\)/;
-    var aCurPrefixes = [];
-    var aReLvls = [
-        /[A-Z]+/ //     Length = 1, Level 0: Capital Letters
-        , /\d+/ //      Length = 2, Level 1: Numbers
-        , /[a-z]+/ //   Length = 3, Level 2: Lowercase Letters
-        , /[ivxlc]+/ // Length = 4, Level 3: Lowercase Roman Numerals
-    ];
-    var sPrevPrefix = "";
 
-    // For each of those children...
+    // Declare & initialize regular expression to recognize outline level prefixes.
+    // Zero or more not-a-word or a whitespace, followed by parentheses that contain at least one character, followed by a newline character
+    var reForOutlineLvlPrefix = /^[\W\s]*\((.+)\)\n/;
+
+    // Declare & initialize an array of arrays of outline level prefixes.
+    var aLvlsOrig = [
+        ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','AA','BB','CC','DD','EE','FF','GG','HH','II','JJ','KK','LL','MM','NN','OO','PP','QQ','RR','SS','TT','UU','VV','WW','XX','YY','ZZ','AAA','BBB','CCC','DDD','EEE','FFF','GGG','HHH','III','JJJ','KKK','LLL','MMM','NNN','OOO','PPP','QQQ','RRR','SSS','TTT','UUU','VVV','WWW','XXX','YYY','ZZZ'],
+        ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60','61','62','63','64','65','66','67','68','69','70','71','72','73','74','75','76','77','78','79','80','81','82','83','84','85','86','87','88','89','90','91','92','93','94','95','96','97','98','99','100'],
+        ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','aa','bb','cc','dd','ee','ff','gg','hh','ii','jj','kk','ll','mm','nn','oo','pp','qq','rr','ss','tt','uu','vv','ww','xx','yy','zz','aaa','bbb','ccc','ddd','eee','fff','ggg','hhh','iii','jjj','kkk','lll','mmm','nnn','ooo','ppp','qqq','rrr','sss','ttt','uuu','vvv','www','xxx','yyy','zzz'],
+        ['i','ii','iii','iv','v','vi','vii','viii','ix','x','xi','xii','xiii','xiv','xv','xvi','xvii','xviii','xix','xx','xxi','xxii','xxiii','xxiv','xxv','xxvi','xxvii','xxviii','xxix','xxx','xxxi','xxxii','xxxiii','xxxiv','xxxv','xxxvi','xxxvii','xxxviii','xxxix','xl','xli','xlii','xliii','xliv','xlv','xlvi','xlvii','xlviii','xlix','l','li','lii','liii','liv','lv','lvi','lvii','lviii','lix','lx','lxi','lxii','lxiii','lxiv','lxv','lxvi','lxvii','lxviii','lxix','lxx','lxxi','lxxii','lxxiii','lxxiv','lxxv','lxxvi','lxxvii','lxxviii'],
+        ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','aa','bb','cc','dd','ee','ff','gg','hh','ii','jj','kk','ll','mm','nn','oo','pp','qq','rr','ss','tt','uu','vv','ww','xx','yy','zz','aaa','bbb','ccc','ddd','eee','fff','ggg','hhh','iii','jjj','kkk','lll','mmm','nnn','ooo','ppp','qqq','rrr','sss','ttt','uuu','vvv','www','xxx','yyy','zzz'],
+        ['i','ii','iii','iv','v','vi','vii','viii','ix','x','xi','xii','xiii','xiv','xv','xvi','xvii','xviii','xix','xx','xxi','xxii','xxiii','xxiv','xxv','xxvi','xxvii','xxviii','xxix','xxx','xxxi','xxxii','xxxiii','xxxiv','xxxv','xxxvi','xxxvii','xxxviii','xxxix','xl','xli','xlii','xliii','xliv','xlv','xlvi','xlvii','xlviii','xlix','l','li','lii','liii','liv','lv','lvi','lvii','lviii','lix','lx','lxi','lxii','lxiii','lxiv','lxv','lxvi','lxvii','lxviii','lxix','lxx','lxxi','lxxii','lxxiii','lxxiv','lxxv','lxxvi','lxxvii','lxxviii']
+    ];
+    // Declare variable to hold a copy of that array of arrays and that will be modified to match the current levels.
+    var aLvlsCur;
+    // Declare variable to hold the current level
+    var iCurLvl;
+    // Declare variables that hold the entire contextual location
+    var aCurLoc;
+    var sCurLoc;
+
+    // Declare variable for indicating whether the level has been found/identified
+    var foundLevel;
+
+    // For each child in div.content div...
     for (var iX = 0; iX < aX.length; iX++) {
         // If we're on a heading...
-        if (aX[iX].nodeName.slice(0, 1) === "H") {
-            // reset everthing
-            sPaddingLeft = "0px";
-            aCurPrefixes = [];
+        if (aX[iX].nodeName.slice(0, 1) === 'H') {
+            // Then reset everthing
+            iCurLvl = 0;
+            // Reset our array of arrays by creating a copy of the original array of levels
+            aLvlsCur = [];
+            for(let iL=0; iL<aLvlsOrig.length; iL++) {
+                aLvlsCur[iL] = aLvlsOrig[iL].slice(0);
+            }
+            aCurLoc = [''];
         }
         // Else, if we're on a "p" that isn't a history "p"...
+        // Note: - History p sections contain information about when the preceding text became law.
+        //       - This is the last else-if, and there is no else.
         else if (aX[iX].matches('p:not([class^=History])')) {
+            try {
 
-            // If the p begins with a outline level prefix
-            var reResult = reForOutlineLvlPrefix.exec(aX[iX].textContent);
-            if (reResult) {
-                var sCurPrefix = reResult[1];
-                // Find the level of the current prefix
-                var iCurLvl;
-                for (var iTestLvl = 0; iTestLvl < aReLvls.length; iTestLvl++) {
-                    if (aReLvls[iTestLvl].test(sCurPrefix)) {
+                // If the p begins with a outline level prefix
+                var reResult = reForOutlineLvlPrefix.exec(aX[iX].textContent);
+                if (reResult) {
+                    foundLevel = false;
 
-                        // If the current prefix might be a letter prefix OR might be a roman numeral...
-                        // (The Roman numerals we are testing for are the letters i, v, x, l, and c.)
-                        if (aCurPrefixes.length > 2 && (
-                            // If the current prefix is "i"
-                            sCurPrefix == "i"
-                            // OR if current prefix is "v" and previous letter prefix is "u" and the previous prefix is "iv" (4)
-                            ||
-                            (sCurPrefix == "v" && aCurPrefixes[2] == "u" && sPrevPrefix == "iv")
-                            // OR if current prefix is "x" and previous letter prefix is "w" and the previous prefix is "ix" (9)
-                            ||
-                            (sCurPrefix == "x" && aCurPrefixes[2] == "w" && sPrevPrefix == "ix")
-                            // OR if current prefix is "l" and previous letter prefix is "k" and the previous prefix is "xlix" (49)
-                            ||
-                            (sCurPrefix == "l" && aCurPrefixes[2] == "k" && sPrevPrefix == "xlix")
-                            // OR if current prefix is "c" and previous letter prefix is "b" and the previous prefix is "xcix" (99)
-                            ||
-                            (sCurPrefix == "c" && aCurPrefixes[2] == "b" && sPrevPrefix == "xcix")
-                        )) {
-                            // Then we need to look ahead to the next prefix to
-                            // determine whether the current prefix is a letter or a Roman numeral.
-                            // For each of the remaining div.content children...
-                            var sNextPrefix;
-                            for (var iLookAhead = iX + 1; iLookAhead < aX.length; iLookAhead++) {
-                                // If we're on a heading...
-                                if (aX[iLookAhead].nodeName.slice(0, 1) === "H") {
-                                    sNextPrefix = "";
-                                    break;
-                                }
-                                // Else, if we're on a "p" that isn't a history "p"...
-                                else if (aX[iLookAhead].matches('p:not([class^=History])')) {
-                                    // If the p begins with a outline level prefix
-                                    var reResult2 = reForOutlineLvlPrefix.exec(aX[iLookAhead].textContent);
-                                    if (reResult2) {
-                                        sNextPrefix = reResult2[1];
-                                        break;
-                                    }
-                                }
-                            } //end for the remaining div.content children
-                            // If we didn't find a next prefix...
-                            if (sNextPrefix == undefined) {
-                                // Uhh.....   this means that we don't know whether the current prefix is a letter or a roman numeral
-                                // So, just presume -- perhaps incorrectly -- that it's a letter.
-                                iCurLvl = iTestLvl;
-                            } else { // else we did find a next prefix
+                    // Get the prefix (from between the parentheses)
+                    var sCurPrefix = reResult[1];
+                    if( sCurLoc === '(B)(29)(b)(ii)(a)(iv)' ) {
+                        debugger;
+                    }
 
-                                // Go back through the if-else-if logic to determine what we need to compare the next prefix to.
+                    // Use the current level and the current array of arrays to determine whether to stay on the current level or go up or down
 
-                                // If the current prefix is "i" and the previous prefix is "h"
-                                if (sCurPrefix == "i") {
-                                    if (sNextPrefix == "ii") {
-                                        iCurLvl = iTestLvl + 1;
-                                    } else {
-                                        iCurLvl = iTestLvl;
-                                    }
-                                }
-                                // If current prefix is "v" and previous letter prefix is "u" and the previous prefix is "iv" (4)
-                                else if (sCurPrefix == "v" && aCurPrefixes[2] == "u" && sPrevPrefix == "iv") {
-                                    if (sNextPrefix == "vi") {
-                                        iCurLvl = iTestLvl + 1;
-                                    } else {
-                                        iCurLvl = iTestLvl;
-                                    }
-                                }
-                                // If current prefix is "x" and previous letter prefix is "w" and the previous prefix is "ix" (9)
-                                else if (sCurPrefix == "x" && aCurPrefixes[2] == "w" && sPrevPrefix == "ix") {
-                                    if (sNextPrefix == "xi") {
-                                        iCurLvl = iTestLvl + 1;
-                                    } else {
-                                        iCurLvl = iTestLvl;
-                                    }
-                                }
-                                // If current prefix is "l" and previous letter prefix is "k" and the previous prefix is "xlix" (49)
-                                else if (sCurPrefix == "l" && aCurPrefixes[2] == "k" && sPrevPrefix == "xlix") {
-                                    if (sNextPrefix == "li") {
-                                        iCurLvl = iTestLvl + 1;
-                                    } else {
-                                        iCurLvl = iTestLvl;
-                                    }
-                                }
-                                // If current prefix is "c" and previous letter prefix is "b" and the previous prefix is "xcix" (99)
-                                else if (sCurPrefix == "c" && aCurPrefixes[2] == "b" && sPrevPrefix == "xcix") {
-                                    if (sNextPrefix == "ci") {
-                                        iCurLvl = iTestLvl + 1;
-                                    } else {
-                                        iCurLvl = iTestLvl;
-                                    }
-                                }
-                            } //end else we did find a next prefix
-                        } else if (aCurPrefixes.length > 3 && aReLvls[3].test(sCurPrefix)) {
-                            iCurLvl = iTestLvl + 1;
-                        } else { // Else we've found the correct level.
-                            iCurLvl = iTestLvl;
+                    // If the current prefix matches the front-most entry at the current level...
+                    if( sCurPrefix === aLvlsCur[iCurLvl][0] ) {
+                        // Then shift off the front-most entry and stay on the current level
+                        aCurLoc[aCurLoc.length-1] = aLvlsCur[iCurLvl].shift();
+                        foundLevel = true;
+                    }
+                    // Else if the current prefix matches the front-most entry at the next level down...
+                    // (if there is a next level down)
+                    else if( iCurLvl+1 < aLvlsCur.length && sCurPrefix === aLvlsCur[iCurLvl+1][0] ) {
+                        // Then go down to that level and shift off the front-most entry at that level
+                        iCurLvl++;
+                        aCurLoc[aCurLoc.length] = aLvlsCur[iCurLvl].shift();
+                        foundLevel = true;
+                    }
+                    // Else, we need to go up level by level to find the level we're at.
+                    else {
+                        let iWasCurLvl = iCurLvl;
+                        let aWasLvlsCur = [];
+                        for(let iL=0; iL<aLvlsCur.length; iL++) {
+                            aWasLvlsCur[iL] = aLvlsCur[iL].slice(0);
                         }
-                        // In either case, we now have the correct level, so break;
-                        break;
-                    } //end if we found a matching level prefix
-                } //end for(each set of level prefixes)
-                if (iCurLvl === undefined) {
-                    console.log("ORC/OAC-Indenting:  Exiting! Cannot determine level of this outline prefix: \"" + sCurPrefix + "\"");
-                    return;
-                }
-                // Revise the array of previous prefixes so it only has levels lower than the current level
-                aCurPrefixes = aCurPrefixes.slice(0, iCurLvl);
-                // Append the current prefix
-                aCurPrefixes.push(sCurPrefix);
+                        let aWasCurLoc = aCurLoc.slice(0);
+                        // for each level that we can go up...
+                        for( iCurLvl = iCurLvl - 1; iCurLvl >= 0; iCurLvl-- ) {
+                            // Reset the prior level down's array
+                            aLvlsCur[iCurLvl+1] = aLvlsOrig[iCurLvl+1].slice(0);
+                            aCurLoc.pop();
+                            // If the current prefix matches the fore-most entry at the current level...
+                            if( sCurPrefix === aLvlsCur[iCurLvl][0] ) {
+                                // Then shift off the front-most entry and stay on the current level
+                                aCurLoc[aCurLoc.length-1] = aLvlsCur[iCurLvl].shift();
+                                foundLevel = true;
+                                break;
+                            }
+                        }
+                        // But if we didn't find the current level while going up..
+                        if( !foundLevel ) {
+                            // Then there must be some mistake in the code (see below).
+                            // Reset the current level.
+                            iCurLvl = iWasCurLvl;
+                            aCurLoc = aWasCurLoc.slice(0);
+                            aLvlsCur = [];
+                            for(let iL=0; iL<aWasLvlsCur.length; iL++) {
+                                aLvlsCur[iL] = aWasLvlsCur[iL].slice(0);
+                            }
+                        }
+                    }
 
-                sPrevPrefix = sCurPrefix;
-            } //end if the p begins with an outline level prefix
+                    // If for some reason we haven't found the level...
+                    if( !foundLevel ) {
+                        // Then there just be some mistake in the code, like there was as of October 29, 2019 at
+                        // http://codes.ohio.gov/oac/3301-51-01v1 where it jumped from (B)(27) to (B)(29) after
+                        // skipping (B)(28)
+                        // On the theory that that could happen again, see if the current prefix matches the
+                        // second-front-most entry of the current level...
+                        if( aLvlsCur[iCurLvl].length > 1 && sCurPrefix === aLvlsCur[iCurLvl][1] ) {
+                            // Then shift off the two front-most entries and stay on the current level
+                            aLvlsCur[iCurLvl].shift();
+                            aCurLoc[aCurLoc.length-1] = aLvlsCur[iCurLvl].shift();
+                        }
+                    }
 
-            // Use the number of levels to set the indenting (pad-left the current "p" element)
-            aX[iX].style.paddingLeft = ((aCurPrefixes.length - 1) * iPixelsOfIndentingPerLevel) + "px";
+                } //end if the p begins with an outline level prefix
 
+                // Use the number of levels to set the indenting (pad-left the current "p" element)
+                aX[iX].style.paddingLeft = (iCurLvl * iPixelsOfIndentingPerLevel) + "px";
+            }
+            catch(e) {
+                console.error('ORC/OAC-Indenting:  ERROR at sCurLoc = "' + sCurLoc + '", sCurPrefix = "' + sCurPrefix + '"');
+                throw e;
+            }
         } //end else-if
 
+        sCurLoc = '('+aCurLoc.join(')(')+')';
+
     } //end for(...)
-    console.log("ORC/OAC-Indenting:  END indenting.");
+    console.info("ORC/OAC-Indenting:  END indenting.");
 } //end vIndentContentByLevel()
